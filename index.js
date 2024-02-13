8eaec8b24da90ae6cb917d4237b2afea55c931ef
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2024-02-13 17:35:10 -0500

    removed commets

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const github = require("@actions/github");
 const main = () => {
   try {
     core.notice("Custom action is working");
-    // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
   } catch (error) {
