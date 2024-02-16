5cbcfdee319be8d79c3ef43978b843bc9535a010
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2024-02-16 11:39:39 -0500

    Run actions

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
-const exec = require("@actions/exec");
+const fetch = require("node-fetch");
+const { exec } = require("@actions/exec");
+const { promises: fs } = require("fs");
 
-const executeCommands = async () => {
+/**
+ * Executes a list of commands.
+ * @param {Array<{ cmd: string, args?: string[] }>} commands - List of commands to execute.
+ * @returns {Promise<void>} - A Promise that resolves when all commands have been executed.
+ */
+const executeCommands = async (commands) => {
   try {
-    core.notice("Start action");
-    
-    await exec.exec("ls");
-    await exec.exec("pwd");
-
+    for (const { cmd, ...args } of commands) {
+      await exec(cmd, null, args);
+    }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(`Error executing commands: ${error.message}`);
   }
 };
 
+/**
+ * Fetches user story data and executes associated actions.
+ * @param {string} baseUrl - The base URL of the API.
+ * @returns {Promise<void>} - A Promise that resolves when actions have been executed.
+ */
+const executeActions = async (baseUrl) => {
+  try {
+    let errorMessage = "";
+    const userId = await fs.readFile(".userid", "utf8");
+    const response = await fetch(
+      `${baseUrl}/api/user-story/user-id?search=${userId}`
+    );
+
+    if (!response.ok) {
+      errorMessage = `Failed to fetch user story data. Status: ${response.status}`;
+      core.setFailed(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    const { result } = await response.json();
+    const story = result?.[0]?.story;
+
+    if (!story?.commands) {
+      errorMessage = "Invalid or missing user story data.";
+      core.setFailed(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    await executeCommands(story.commands);
+  } catch (error) {
+    core.setFailed(`Error executing actions: ${error.message}`);
+  }
+};
+
+/**
+ * Main entry point of the program.
+ * @returns {Promise<void>} - A Promise that resolves when execution is complete.
+ */
 const main = async () => {
   try {
-    await executeCommands();
+    const baseUrl = "https://dbeb-191-106-237-3.ngrok-free.app";
+    await executeActions(baseUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
